88fb46970a655b6e9551365ad6257e8a75a5f62b
M. Mert Yildiran <me@mertyildiran.com>
2022-10-18 16:41:09 +0300

    Update `GITHUB_EDIT_URL` and `COMMUNITY_INVITE_URL`

--- src/config.ts
+++ src/config.ts
@@ -32,9 +32,9 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/kubeshark/docs/tree/main`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://kubeshark.co/chat`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
