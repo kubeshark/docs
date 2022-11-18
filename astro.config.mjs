09531631c03011e10f4102cfdd2ace9afe05399b
M. Mert Yildiran <me@mertyildiran.com>
2022-11-18 23:36:26 +0300

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,26 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 import codeExtra from 'remark-code-extra';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), sitemap()],
   markdown: {
     remarkPlugins: [[codeExtra, {
       // Add a link to stackoverflow if there is one in the meta
       transform: node => ({
-        after: [
-          {
-            type: 'element',
-            tagName: 'button',
-            children: []
-          }
-        ]
+        after: [{
+          type: 'element',
+          tagName: 'button',
+          children: []
+        }]
       })
-    }]],
+    }]]
   },
-	site: `http://astro.build`,
+  site: `http://astro.build`
 });
