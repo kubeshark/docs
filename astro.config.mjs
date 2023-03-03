c0b9a6748f85caa88b6dcf467f6897ae07324ccd
M. Mert Yildiran <me@mertyildiran.com>
2023-03-03 21:26:59 +0300

    :bug: Bring back the syntax highlighting to Markdown code snippets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,17 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-import codeExtra from 'remark-code-extra';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react(), sitemap()],
-  markdown: {
-    remarkPlugins: [[codeExtra, {
-      // Add a link to stackoverflow if there is one in the meta
-      transform: node => ({
-        after: [{
-          type: 'element',
-          tagName: 'button',
-          children: []
-        }]
-      })
-    }]]
-  },
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    // Sitemap
+    sitemap(),
+  ],
   site: `http://astro.build`
 });
